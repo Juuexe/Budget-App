51dadb348798478600cdeade5a5401d8235e6757
Juuexe <ayoini13@gmail.com>
2026-05-16 21:40:15 -0500

    Installed dependencies and linked frontend and backend

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
